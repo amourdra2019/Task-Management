a231f48fa40e6e5803d103aa8934c9ecd9fafc0d
unknown <awawouraye586@gmail.com>
2024-07-08 13:53:14 +0200

    inf466

---
+++ frontend/task-manager-app/src/component/CalendarView.js
@@ -0,0 +1,53 @@
+
+import "./CalendarView.css"
+import Calendar from "./Calendar";
+import CalendarTaskRow from "./CalendarTaskRow";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+export default function CalendarView (){
+    const [date,setDate] = useState();
+    const [uncompletedTasks,setuncompletedTasks] = useState();
+    const [completedTasks,setcompletedTasks] = useState();
+    const getTasks = async ()=>{ 
+        try {
+            if(date){
+
+                const {data} = await axios.get(`http://localhost:3001/api/tasks/date/${date}`);
+                if(data.success === true){
+                    const uncompleted =  
+                    data.data.filter(e=>e.completed === false)
+                    .map((e,i)=> {
+                             return <CalendarTaskRow key={i} {...e}/>;
+                        
+                    });
+                    setuncompletedTasks([...uncompleted]);
+                    const completed =data.data.filter(e=>e.completed === true)
+                    .map((e,i)=> {
+                             return <CalendarTaskRow key={i} {...e}/>;
+                    });
+                    setcompletedTasks([...completed]);            
+        
+                }
+            }
+            
+        } catch (error) {
+            
+        }
+      
+
+    };
+        useEffect(()=>{
+            getTasks();
+        },[date])
+    return (
+        <div id="calendar_view_container">
+        <Calendar dateState={setDate} />
+        <div id="calendar_tasks_container">
+            {completedTasks}
+            {uncompletedTasks}
+        </div>
+        </div>
+    )
+}
